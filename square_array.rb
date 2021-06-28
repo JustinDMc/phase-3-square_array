2db774a888147e459be3f28bce69dcd8b6cb8227
Justin <jdanielmcarthur@msn.com>
2021-06-28 14:44:26 -0700

    commit tree 16f460e70a58b43ee36118aaf37e4ef1f4fe49fb

--- square_array.rb
+++ square_array.rb
@@ -1,3 +1,7 @@
-def square_array(array)
-  # your code here
+numbers = ["6", "7", "8", "9", "10"]
+
+def square_array(numbers)
+  new_numbers = []
+  numbers.each { |number| new_numbers << number ** 2 }
+  new_numbers
 end
